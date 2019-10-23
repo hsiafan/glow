e4a0be413c072235163e5dc5e6c81e2085df5796
dongliu <dongliu@kuaishou.com>
2019-10-23 13:40:36 +0800

    add time duration creation utils

--- timex/time_utils.go
+++ timex/time_utils.go
@@ -11,3 +11,28 @@ func EpochMills(millis int64) time.Time {
 func Epoch(secs int64) time.Time {
 	return time.Unix(secs, 0)
 }
+
+// MillisOf make creating time.Duration from milli seconds easier
+func MillisOf(millis int) time.Duration {
+	return time.Millisecond * time.Duration(millis)
+}
+
+// MicrosOf make creating time.Duration from micro seconds easier
+func MicrosOf(micros int) time.Duration {
+	return time.Microsecond * time.Duration(micros)
+}
+
+// SecondsOf make creating time.Duration from seconds easier
+func SecondsOf(seconds int) time.Duration {
+	return time.Microsecond * time.Duration(seconds)
+}
+
+// MinutesOf make creating time.Duration from minutes easier
+func MinutesOf(minutes int) time.Duration {
+	return time.Microsecond * time.Duration(minutes)
+}
+
+// HoursOf make creating time.Duration from hours easier
+func HoursOf(hours int) time.Duration {
+	return time.Hour * time.Duration(hours)
+}
