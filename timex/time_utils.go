be293d95936991e2e1529082370b7f2ecb31bd92
dongliu <dongliu@kuaishou.com>
2019-10-23 13:42:33 +0800

    refactor timex

--- timex/time_utils.go
+++ timex/time_utils.go
@@ -2,6 +2,10 @@ package timex
 
 import "time"
 
+const SimpleTime = "2006-01-02 15:04:05"
+const SimpleTimeMills = "2006-01-02 15:04:05.000"
+const SimpleDate = "2006-01-02"
+
 // EpochMills convert unix epoch mills to time
 func EpochMills(millis int64) time.Time {
 	return time.Unix(0, millis*int64(time.Millisecond))
@@ -10,29 +14,4 @@ func EpochMills(millis int64) time.Time {
 // Epoch convert unix epoch seconds to time
 func Epoch(secs int64) time.Time {
 	return time.Unix(secs, 0)
-}
-
-// MillisOf make creating time.Duration from milli seconds easier
-func MillisOf(millis int) time.Duration {
-	return time.Millisecond * time.Duration(millis)
-}
-
-// MicrosOf make creating time.Duration from micro seconds easier
-func MicrosOf(micros int) time.Duration {
-	return time.Microsecond * time.Duration(micros)
-}
-
-// SecondsOf make creating time.Duration from seconds easier
-func SecondsOf(seconds int) time.Duration {
-	return time.Microsecond * time.Duration(seconds)
-}
-
-// MinutesOf make creating time.Duration from minutes easier
-func MinutesOf(minutes int) time.Duration {
-	return time.Microsecond * time.Duration(minutes)
-}
-
-// HoursOf make creating time.Duration from hours easier
-func HoursOf(hours int) time.Duration {
-	return time.Hour * time.Duration(hours)
 }
--- timex/time_utils_test.go
+++ timex/time_utils_test.go
@@ -21,3 +21,10 @@ func TestEpoch(t *testing.T) {
 	assert.Equal(t, time.Month(10), m)
 	assert.Equal(t, 9, d)
 }
+
+func TestFormat(t *testing.T) {
+	tm := EpochMills(1570612959000)
+	assert.Equal(t, "2019-10-09", tm.Format(SimpleDate))
+	assert.Equal(t, "2019-10-09 17:22:39", tm.Format(SimpleTime))
+	assert.Equal(t, "2019-10-09 17:22:39.000", tm.Format(SimpleTimeMills))
+}
