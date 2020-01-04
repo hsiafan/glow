ee50530d0a77fb0690190343cc62610af9a9f402
dongliu <dongliu@kuaishou.com>
2020-01-04 08:48:27 +0800

    rename

--- timex/duration_utils.go
+++ timex/duration_utils.go
@@ -2,27 +2,27 @@ package timex
 
 import "time"
 
-// MillisOf make creating time.Duration from milli seconds easier
-func MillisOf(millis int) time.Duration {
+// OfMillis make creating time.Duration from milli seconds easier
+func OfMillis(millis int) time.Duration {
 	return time.Millisecond * time.Duration(millis)
 }
 
-// MicrosOf make creating time.Duration from micro seconds easier
-func MicrosOf(micros int) time.Duration {
+// OfMicros make creating time.Duration from micro seconds easier
+func OfMicros(micros int) time.Duration {
 	return time.Microsecond * time.Duration(micros)
 }
 
-// SecondsOf make creating time.Duration from seconds easier
-func SecondsOf(seconds int) time.Duration {
+// OfSeconds make creating time.Duration from seconds easier
+func OfSeconds(seconds int) time.Duration {
 	return time.Microsecond * time.Duration(seconds)
 }
 
-// MinutesOf make creating time.Duration from minutes easier
-func MinutesOf(minutes int) time.Duration {
+// OfMinutes make creating time.Duration from minutes easier
+func OfMinutes(minutes int) time.Duration {
 	return time.Microsecond * time.Duration(minutes)
 }
 
-// HoursOf make creating time.Duration from hours easier
-func HoursOf(hours int) time.Duration {
+// OfHours make creating time.Duration from hours easier
+func OfHours(hours int) time.Duration {
 	return time.Hour * time.Duration(hours)
 }
