b1c7180f3e2a9e500e35018ad6b53a6ed517f5fa
dongliu <dongliu@kuaishou.com>
2020-01-31 19:30:25 +0800

    improve encoding utils

--- charsetx/charset.go
+++ charsetx/charset.go
@@ -1,26 +1,21 @@
 package charsetx
 
 import (
-	"bytes"
 	"golang.org/x/text/encoding"
-	"golang.org/x/text/transform"
-	"io/ioutil"
 )
 
 type Encoding int
 
 // convert string to bytes with specific encoding
 func EncodeString(str string, encoding encoding.Encoding) ([]byte, error) {
-	reader := transform.NewReader(bytes.NewReader([]byte(str)), encoding.NewEncoder())
-	return ioutil.ReadAll(reader)
+	return encoding.NewEncoder().Bytes([]byte(str))
 }
 
 // convert bytes to string with specific encoding
 func DecodeString(data []byte, encoding encoding.Encoding) (string, error) {
-	reader := transform.NewReader(bytes.NewReader(data), encoding.NewDecoder())
-	utf8Data, err := ioutil.ReadAll(reader)
+	data, err := encoding.NewDecoder().Bytes(data)
 	if err != nil {
 		return "", err
 	}
-	return string(utf8Data), err
+	return string(data), nil
 }
